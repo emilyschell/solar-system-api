b4927abd1b6ce7bd2b67830dba99907df70ced0f
xiomara <xrodri98@gmail.com>
2022-04-29 14:43:14 -0400

    updated modules and app for GET and POST

---
+++ app/models/planet.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    moons = db.Column(db.Integer)
+
+def to_json(self):
+        return {
+                "id": self.id,
+                "name": self.name,
+                "description": self.description,
+                "moons": self.moons
+            } 
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,49 @@
-from flask import Blueprint, jsonify, abort, make_response
+from flask import Blueprint, jsonify, abort, make_response, request
+from app.models.planet import Planet
+from app import db
 
 
-class Planet:
-    def __init__(self, name, id, description, moons):
-        self.name = name
-        self.id = id
-        self.description = description
-        self.moons = moons
 
-    def to_json(self):
-        return {
-                "id": self.id,
-                "name": self.name,
-                "description": self.description,
-                "moons": self.moons
-            }
+# class Planet:
+#     def __init__(self, name, id, description, moons):
+#         self.name = name
+#         self.id = id
+#         self.description = description
+#         self.moons = moons
 
-planets = [
-    Planet("Mercury", 1,
-           "Looks gray, so gray. A year is just 88 days long. Smallest planet.", 0),
-    Planet("Venus", 2, "Kindof ivory? A day is longer than a year.", 0),
-    Planet("Earth", 3, "Moist and oxygenated. Overrun with life.", 1)
-]
+#     def to_json(self):
+#         return {
+#                 "id": self.id,
+#                 "name": self.name,
+#                 "description": self.description,
+#                 "moons": self.moons
+#             }
+
+# planets = [
+#     Planet("Mercury", 1,
+#            "Looks gray, so gray. A year is just 88 days long. Smallest planet.", 0),
+#     Planet("Venus", 2, "Kindof ivory? A day is longer than a year.", 0),
+#     Planet("Earth", 3, "Moist and oxygenated. Overrun with life.", 1)
+# ]
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+# CREATE PLANET
+@planets_bp.route("",  methods=["POST"])
+def create_planet():
+    request_body = request.get_json()
+
+    new_planet = Planet(
+        name=request_body['name'],
+        description=request_body['description'],
+        moons=request_body['moons']
+        
+    )
+    
+    db.session.add(new_planet)
+    db.session.commit()
+
+    return make_response(f"Planet {new_planet.name} has been succesfully created!",201)
 
 def validate(planet_id):
     try:
@@ -31,18 +51,20 @@ def validate(planet_id):
     except:
         return abort(make_response({"message" :f"planet {planet_id} invalid"}, 400))
 
-    for planet in planets:
-        if planet.id == planet_id:
-            return planet
+    planet = Planet.query.get(planet_id)
+    if not planet:
 
-    return abort(make_response({"message" :f"planet {planet_id} not found"}, 404))
+        return abort(make_response({"message" :f"planet {planet_id} not found"}, 404))
+    return planet
 
-
+# GET ALL PLANETS
 @planets_bp.route("", methods=["GET"])
 def read_all_planets():
     response = []
+    planets = Planet.query.all()
     for planet in planets:
         response.append(planet.to_json())
+
     return jsonify(response)
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
